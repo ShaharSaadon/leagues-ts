a6e5cf068823aba39fa0243fdcecde15a8180323
Shahar saadon <shahars@moveo.co.il>
2023-07-20 12:05:40 +0300

    last version

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,86 @@
 const leagueNames: string[] = ['English Premier League', 'Israeli Premier League', 'UEFA Europa League', 'Spanish La Liga', 'FIFA World Cup'];
-const leaguesDiv: HTMLElement | null = document.getElementById("leagues");
+const END_POINT: string = 'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l='
 
-interface ITeam {
+interface IFootballTeam {
     idTeam: string;
     strTeam: string;
-    strTeamBadge: string;
+    strTeamBadge?: string;
 }
 
-async function fetchTeamsByLeague(leagueName: string) {
+function initialLeagues(): void {
+    const mainDiv: HTMLElement | null = document.getElementById("main");
+    const leagueTitle: HTMLElement | null = document.getElementById("league-title");
+    const backButton: HTMLElement | null = document.querySelector('.back-button');
+
+    if(leagueTitle) leagueTitle.innerHTML=``
+
+
+    if (backButton) {
+        backButton.remove();
+    }
+
+    if (mainDiv) {
+        mainDiv.innerHTML = '';
+        leagueNames.forEach((leagueName, i) => {
+            const btn = createElement('button', leagueName);
+            btn.id = `league-button-${i}`;
+            mainDiv.appendChild(btn);
+            btn.addEventListener('click', () => displayTeamsByLeague(leagueName));
+        });
+
+        
+    
+    } else {
+        console.error('Could not find element with id "main"');
+    }
+}
+
+function createElement(tag: string, text: string): HTMLElement {
+    const element = document.createElement(tag);
+    element.textContent = text;
+    return element;
+}
+
+async function displayTeamsByLeague(leagueName: string): Promise<void> {
     try {
-        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=${leagueName}`);
-        const data = await response.json();
+        const teams = await fetchTeamsByLeague(leagueName);
+        renderTeams(teams, leagueName);
 
-        if (Array.isArray(data.teams)) {
-            if (leaguesDiv) {
-                leaguesDiv.innerHTML = '';
-                const h4 = document.createElement('h4');
-                h4.textContent = `Teams in ${leagueName}:`;
-                leaguesDiv.appendChild(h4);
-
-                data.teams.forEach((team: any) => {
-                    if ('strTeam' in team) {
-                        const p = document.createElement('p');
-                        p.textContent = team.strTeam;
-                        leaguesDiv.appendChild(p);
-                    }
-                });
-
-                const backButton = document.createElement('button');
-                backButton.textContent = 'Back';
-                backButton.addEventListener('click', displayLeagueNames);
-                leaguesDiv.appendChild(backButton);
-            }
-        } else {
-            throw new Error('Invalid data format received from the server');
-        }
+        const backButton = createElement('button', 'Back');
+        backButton.addEventListener('click', initialLeagues);
+        backButton.className = 'back-button';
+        document.body.appendChild(backButton);
+        
     } catch (error) {
         console.error(`Failed to fetch teams for league: ${leagueName}. Error: ${error}`);
     }
 }
 
-function displayLeagueNames() {
-    if (leaguesDiv) {
-        leaguesDiv.innerHTML = '';
-        leagueNames.forEach((leagueName, i) => {
-            const btn = document.createElement('button');
-            btn.textContent = leagueName;
-            btn.id = `league-button-${i}`;
-            leaguesDiv.appendChild(btn);
-            btn.addEventListener('click', () => fetchTeamsByLeague(leagueName));
-        });
+async function fetchTeamsByLeague(leagueName: string): Promise<IFootballTeam[]> {
+    const response = await fetch(`${END_POINT}${leagueName}`);
+    const data = await response.json();
+
+    if (Array.isArray(data.teams)) {
+        return data.teams;
     } else {
-        console.error('Could not find element with id "leagues"');
+        throw new Error('Invalid data format received from the server');
     }
 }
 
-displayLeagueNames(); // Initial display
+function renderTeams(teams: IFootballTeam[], leagueName: string): void {
+    const mainDiv: HTMLElement | null = document.getElementById("main");
+    if (!mainDiv) return;
+    const leagueTitle: HTMLElement | null = document.getElementById("league-title");
+
+    mainDiv.innerHTML = '';
+    if(leagueTitle) leagueTitle.innerHTML=`Teams in ${leagueName}`
+    teams.forEach((team: IFootballTeam) => {
+        const p = createElement('button', team.strTeam);
+        mainDiv.appendChild(p);
+    });
+
+    const backButton = createElement('button', 'Back');
+    backButton.addEventListener('click', initialLeagues);
+}
+
+initialLeagues();
